e1577e1ab475a73e1ff867461332007d8ce175a8
OCT <45591704+Johnny-Demo@users.noreply.github.com>
2022-07-21 13:48:46 +0800

    Delete cni.sh

--- cni.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Author: Johny
-# Email: xxx@163.com
-# Date: 05/25/2022
-# Filename: cni.sh
-
-
-# 安装网络插件并查看集群状态
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
