4b93ccbe2920296730830affba6645b0733ea1f5
OCT <45591704+Johnny-Demo@users.noreply.github.com>
2023-10-27 15:21:03 +0400

    Update cni.sh

--- cni.sh
+++ cni.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 安装网络插件
-git clone https://github.com/flannel-io/flannel.git && kubectl apply -f /root/deploy-Uat-three/flannel/Documentation/kube-flannel.yml  
+git clone https://github.com/flannel-io/flannel.git && kubectl apply -f /root/deploy/flannel/Documentation/kube-flannel.yml  
